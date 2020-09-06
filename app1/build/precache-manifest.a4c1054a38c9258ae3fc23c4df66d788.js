91499287dce6851d8f7ec0f4cf82656e1744d7a8
C0ffeeMachine <yashshukla195@gmail.com>
2020-09-07 03:50:10 +0530

    deployment changes

---
+++ app1/build/precache-manifest.a4c1054a38c9258ae3fc23c4df66d788.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3c0e264c539750411d1030c7b74985",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da865e59043eaeb5bafe",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "3482201a5cf96e07646b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da865e59043eaeb5bafe",
+    "url": "/static/js/2.a7f0fc9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a7f0fc9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3482201a5cf96e07646b",
+    "url": "/static/js/main.d9ad4dc2.chunk.js"
+  },
+  {
+    "revision": "a3d2bf21716328fc74a3",
+    "url": "/static/js/runtime-main.d8499093.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
